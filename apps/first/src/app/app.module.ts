5051c00fa2acb8248d80b8ad8d041c802cd44016
Sabith Pocker <sabithpocker@gmail.com>
2021-06-21 03:07:34 +0530

    fix dependancy

--- apps/first/src/app/app.module.ts
+++ apps/first/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ScullyLibModule],
+  imports: [
+    BrowserModule, 
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    ScullyLibModule,ScullyLibModule],
   providers: [],
   bootstrap: [AppComponent],
 })
